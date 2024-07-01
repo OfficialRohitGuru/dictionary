c559b83a8100ac33b133ed0aaae9050bfd548e05
Rohit Guru <officialrohitguru@gmail.com>
2024-07-02 02:31:46 +0530

    dictionary

---
+++ src/Dictionary.jsx
@@ -0,0 +1,49 @@
+import React,{useState} from "react";
+
+const Dict= 
+[
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+
+
+export default function Dictionary(){
+    
+    const [dispaly,setDisplay]= useState("")
+    const [defi,setDifi]= useState("")
+    const [search,setSearch]= useState(false)
+
+    function handleChange(e){
+        setSearch(false)
+        let type=(e.target.value)
+        setDisplay(type)
+        let result = Dict.find(item => type === item.word)
+        if(result){
+            setDifi(result.meaning)
+        }else{
+            setDifi("Word not found in the dictionary.")
+        }
+      
+        
+       
+    }
+    function handleClick(){
+        setSearch(true)
+    }
+
+
+    return (<>
+    <h2>DICTIONARY APP</h2>
+    <input type="text" value={dispaly} onChange={handleChange} placeholder="Search for a word.." />
+    <button onClick={handleClick}>Search</button>
+    <h5>Definition: </h5>
+    {search && <p>{defi}</p>}
+
+    </>);
+}
